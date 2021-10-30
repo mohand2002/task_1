14a6471dce21cbfb2365f64062c12a49254500c7
mohand2002 <93428351+mohand2002@users.noreply.github.com>
2021-10-30 20:24:55 +0200

    Add files via upload

---
+++ task1.m
@@ -0,0 +1,22 @@
+a=[-7 5 -9;2 -1 2;1 -1 2];
+b=[16 3 2 13 ;5 10 11 8;9 6 7 12;4 15 14 1];
+c=[4 2 -3;7 -7 9;3 -5 6];
+d=[6 3 2;2 12 -7;-1 6 2;-5 15 11];
+e=[5 ;5;5;5;5;5;5 ];
+3*a-5*c
+%7*a+2*b Error matrix dimensions must agree.
+c*a
+c*d'
+zeros(5) 
+zeros(6,5)
+ones(5)
+ones(6,5)
+size(d)
+zeros(size(d))
+diag([1 2 3 4])
+eye(5)
+%[a,b] Error dimensions of matrices being concatenated are not consistent.
+%[a;b] Error dimensions of matrices being concatenated are not consistent.
+diag([5 5 5 5 5 5 5 ])
+a(3,:) 
+a(:,2)
